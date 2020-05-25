03755c8fa312e8dcfb8e02b2c59e6b21e09779d9
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-05-25 00:50:29 -0600

    search by tags

--- ProyectoPython/Conexion.py
+++ ProyectoPython/Conexion.py
@@ -17,10 +17,24 @@ class Conexion:
         with self.driver.session() as session:
             session.write_transaction(self._delete_game, name)
 
+    def searchtags(self, tags):
+        with self.driver.session() as session:
+            games = session.read_transaction(self._search_by_tags, tags)
+            return games
+
     @staticmethod
     def _create_node(tx, name, tags):
-        tx.run("CREATE (:Juegos {name: $name, tags: $tags, n:0})", name=name, tags=tags)
+        tx.run("CREATE (:Juegos {name: $name, tags: $tags})", name=name, tags=tags)
 
     @staticmethod
     def _delete_game(tx, name):
         tx.run("MATCH (n:Juegos) WHERE n.name=$name DETACH DELETE n", name=name)
+
+
+    @staticmethod
+    def _search_by_tags(tx, tags):
+        juegos = []
+        for tag in tags:
+            for elemento in tx.run("MATCH (n:Juegos) WHERE $tag IN n.tags RETURN n.name", tag=tag):
+                juegos.append(elemento["n.name"])
+        return juegos
--- ProyectoPython/Controller.py
+++ ProyectoPython/Controller.py
@@ -4,8 +4,9 @@ class Controller:
 
     def __init__(self):
         self.conexion = Conexion("bolt://localhost:7687", "neo4j", "Proyecto123")
-        self.conexion.createNode("Call of Duty 1", ["accion", "aventura", "shooter"])
-        #self.conexion.deleteNode("Call_of_Duty")
+        #self.conexion.createNode("Call of Duty 1", ["first person", "aventura", "shooter"])
+        #self.conexion.deleteNode("Call of Duty 1")
+        print(self.conexion.searchtags(["aventura"]))
         self.conexion.close()
 
 
