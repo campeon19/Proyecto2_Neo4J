dc2b8a021bdf60b03605bbe4a24d1d84223c7b39
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-05-24 22:09:05 -0600

    python

--- src/NeoBase.java
+++ src/NeoBase.java
@@ -1,4 +1,50 @@
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
 
-public class NeoBase {
+public class NeoBase implements AutoCloseable{
+	
+	private final Driver driver;
+	
+	public NeoBase(String uri, String user,String password) {
+		driver = GraphDatabase.driver(uri,AuthTokens.basic(user, password));
+	}
+
+	@Override
+	public void close() throws Exception {
+		driver.close();
+		
+	}
+
+	public void printGreeting( final String message )
+    {
+        try ( Session session = driver.session() )
+        {
+            String greeting = session.writeTransaction( new TransactionWork<String>()
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    Result result = tx.run( "CREATE (a:Greeting) " +
+                                                     "SET a.message = $message " +
+                                                     "RETURN a.message + ', from node ' + id(a)" );
+                    return result.single().get( 0 ).asString();
+                }
+            } );
+            System.out.println( greeting );
+        }
+    }
+
+    public static void main( String... args ) throws Exception
+    {
+        try (NeoBase greeter = new NeoBase( "bolt://localhost:7687", "neo4j", "password" ) )
+        {
+            greeter.printGreeting( "hello, world" );
+        }
+    }
 
 }
