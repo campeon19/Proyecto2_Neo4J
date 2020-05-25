83294bf14c7136fd4fcd2b7036990df72255fd4c
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-05-24 22:35:04 -0600

    conexion

--- ProyectoPython/Conexion.py
+++ ProyectoPython/Conexion.py
@@ -9,22 +9,7 @@ class Conexion:
     def close(self):
         self.driver.close()
 
-    # Envia el codigo del metodo '_create_person' a la base de datos.
-    def save_name(self, name, age, preference):
-        with self.driver.session() as session:
-            session.write_transaction(self._create_person, name, age, preference)
-
-    def save_place(self, place_name, location):
-        with self.driver.session() as session_save_place:
-            session_save_place.write_transaction(self._create_place, place_name, location)
-
-    def find_user(self, username):
-        with self.driver.session() as session_find_user:
-            session_find_user.read_transaction(self._search_user, username)
-
-    def delete_user(self, username):
-        with self.driver.session() as session_delete_user:
-            session_delete_user.write_transaction(self._delete_user, username)
 
 
-conexion = Conexion("bolt://localhost:7687", "neo4j", "prueba123")
+
+
---
+++ ProyectoPython/Controller.py
@@ -0,0 +1,3 @@
+from Conexion import Conexion
+
+Conexion("bolt://localhost:7687", "neo4j", "Proyecto123")
