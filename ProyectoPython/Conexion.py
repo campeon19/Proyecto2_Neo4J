5a9792669134834e0bd80b6791620fbb9f8d2f1e
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-05-25 00:01:09 -0600

    add delete

--- ProyectoPython/Conexion.py
+++ ProyectoPython/Conexion.py
@@ -9,7 +9,18 @@ class Conexion:
     def close(self):
         self.driver.close()
 
+    def createNode(self, name, tags):
+        with self.driver.session() as session:
+            session.write_transaction(self._create_node, name, tags)
 
+    def deleteNode(self, name):
+        with self.driver.session() as session:
+            session.write_transaction(self._delete_game, name)
 
+    @staticmethod
+    def _create_node(tx, name, tags):
+        tx.run("CREATE (:Juegos {name: $name, tags: $tags, n:0})", name=name, tags=tags)
 
-
+    @staticmethod
+    def _delete_game(tx, name):
+        tx.run("MATCH (n:Juegos) WHERE n.name=$name DETACH DELETE n", name=name)
--- ProyectoPython/Controller.py
+++ ProyectoPython/Controller.py
@@ -4,3 +4,9 @@ class Controller:
 
     def __init__(self):
         self.conexion = Conexion("bolt://localhost:7687", "neo4j", "Proyecto123")
+        self.conexion.createNode("Call of Duty 1", ["accion", "aventura", "shooter"])
+        #self.conexion.deleteNode("Call_of_Duty")
+        self.conexion.close()
+
+
+Controller()
