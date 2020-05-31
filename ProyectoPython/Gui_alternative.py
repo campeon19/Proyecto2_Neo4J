dc1d4d06afb520afa998d85323c9dcbfa35a094c
Jose Hurtarte <JJHH121106@gmail.com>
2020-05-30 22:12:32 -0600

    Finalizacion de la GUI

--- ProyectoPython/Gui_alternative.py
+++ ProyectoPython/Gui_alternative.py
@@ -1,50 +1,64 @@
 from tkinter import *
 from Conexion import Conexion
+from tkinter import ttk
 
+# raiz de la interfaz grafica
 root = Tk()
 
-# variable is stored in the root object
-root.counter = 0
 
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#CAMBIAR PARA CADA BASE DE DATOS
 root.conexion=Conexion("bolt://localhost:7687", "neo4j", "Bruhxd")
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#Aqui se establece el titulo de el juego
 root.title("GameRecommend")
 root.iconbitmap("recomendation_logo.ico")
 
-programFrame = Frame(root,width="750",height="600" )
+#Frame y propiedades
+programFrame = Frame(root,width="400",height="600" )
 programFrame.pack(fill="both",expand="True")
 programFrame.config(cursor="pirate")
 programFrame.config(bg = "#ffffff")
 
 
-
+#Funcion que se manda a llamar con el boton asociado
 def clicked():
-    root.counter += 1
-    resultado = root.conexion.recomendaciones_juego(EntryTextJuego.get(),SpinnerMinimo.get(),SpinnerMaximo.get(),EntryTextConsola.get())
+    resultado = root.conexion.recomendaciones_juego(EntryTextJuego.get(),SpinnerMinimo.get(),SpinnerMaximo.get(),str(SeleccionConsola.get()))
     indiceJuegos = 1
     textoRecomendacion=""
+
+    #progra defensiva por si meten algo que no existe o si no encuetra recomendaciones
     if len(resultado) > 0:
         for n in resultado:
             textoRecomendacion+="\n"+str(indiceJuegos)+") "+str(n)
             indiceJuegos+=1
     else:
-        textoRecomendacion="Vaya!\n no se han encontrado recomendaciones para los parametros dados"
+        textoRecomendacion="¡Vaya!\n ¡No se han encontrado \nrecomendaciones para los\n parametros dados!"
 
-    #L['text'] = textoRecomendacion
+   #Se establece el textbox dependiendo de los campos que se pusieron
+
+    # Se habilita para poder escribir
     TextRecommendation.config(state = "normal")
+    # Se hace la insersion
     TextRecommendation.delete(1.0,END)
     TextRecommendation.insert(INSERT,textoRecomendacion)
     TextRecommendation.config(state="disabled")
-    #aqui se debe de poner lo que va a ir en el texto
+
 
 
 #BOTON
-b = Button(programFrame, text="Obtener Recomendacion", command=clicked)
-b.place(x=150,y=250)
+b = Button(programFrame, text="Obtener Recomendacion", font = ('Sans','10','bold'),command=clicked)
+imagetest = PhotoImage(file="image_control.png")
+b.config(image = imagetest,compound = BOTTOM)
+b.place(x=125,y=250)
 b.config(bg = "#e3f2fd")
 
-
+#TEXTBOX de el resultado
 TextRecommendation =Text(programFrame,height=10,width=30)
-TextRecommendation.place(x=50,y=350)
+TextRecommendation.place(x=90,y=420)
 TextRecommendation.config(state ="disable")
 #COLOR TEXTBOX
 TextRecommendation.config(bg="#b1bfca")
@@ -63,8 +77,12 @@ LabelJuego.place(x=50,y=50)
 #COLOR LABEL JUEGO
 LabelJuego.config(bg = "#ffffff")
 
+
 EntryTextJuego = Entry(programFrame)
 EntryTextJuego.place(x=50,y=100)
+EntryTextJuego.config(bg="#b1bfca")
+
+
 
 
 #campo para la plataforma en la que juega el usuario
@@ -74,8 +92,17 @@ LabelConsola.place(x=250,y=50)
 #COLOR LABEL CONSOLA
 LabelConsola.config(bg = "#ffffff")
 
-EntryTextConsola = Entry(programFrame)
-EntryTextConsola.place(x=250,y=100)
+
+#Configuracion para el combobox
+
+#Variable asociada con el combobox
+SeleccionConsola = StringVar()
+
+#combobox de consola
+ComboboxConsola = ttk.Combobox(width=20,textvariable=SeleccionConsola)
+ComboboxConsola['values']=("PS4","Xbox One","PC","Smartphone","Xbox 360","Wii","Switch","PS3")
+ComboboxConsola.place(x=250,y=100)
+
 
 
 #Spinner para las horas minimas que juega el usuario
@@ -88,6 +115,8 @@ LabelHorasMin.config(bg = "#ffffff")
 #SPINNER MINIMO
 SpinnerMinimo = Spinbox(programFrame,from_=1,to=12)
 SpinnerMinimo.place(x=50,y=200)
+#COLOR SPINNER MINIMO
+SpinnerMinimo.config(bg="#b1bfca")
 
 #Spinner para las horas maximas que juega el usuario
 LabelHorasMax = Label(programFrame, text=str("Ingrese las horas MAXIMAS\n que juga a la semana"))
@@ -99,5 +128,8 @@ LabelHorasMax.config(bg = "#ffffff")
 #SPINNER MAXIMO
 SpinnerMaximo = Spinbox(programFrame,from_=1,to=12)
 SpinnerMaximo.place(x=250,y=200)
+#COLOR SPINNER MAXIMO
+SpinnerMaximo.config(bg="#b1bfca")
 
+#loop de repeticion para actualizar la vista
 root.mainloop()
